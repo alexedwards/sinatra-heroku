3355509107fdb56a2d43a52f47ff8ef6a287915a
Alex Edwards <ajmedwards@gmail.com>
2013-01-09 00:49:49 +0000

    add: boilerplate code to redirect all requests to www

--- main.rb
+++ main.rb
@@ -5,6 +5,12 @@ configure :development do
   Sass::Plugin.options[:style] = :compressed
 end
 
+configure :production do
+  before do
+    redirect(to("#{ request.scheme }://www.#{ request.host }#{ request.path }"), 301) unless (/^www/ =~ request.host)
+  end
+end
+
 get	'/' do
 	erb :index
 end
